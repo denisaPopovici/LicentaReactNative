ebda904fed1e7b9fde37b39d49e012e072d9c8b0
Sebastian Ciocan <sebastianciocan@Sebastians-MacBook-Pro.local>
2024-04-27 17:39:57 +0300

    Initial maintainability commit for code smell fixing

--- App.js
+++ App.js
@@ -1,17 +1,9 @@
-//import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
-// import {StackNavigator} from 'react-navigation';
-import {StyleSheet, Text, View, Button, AsyncStorage} from 'react-native';
-//import { Marker, Callout } from 'react-native-maps';
-//import MapView, { PROVIDER_GOOGLE }  from 'react-native-maps';
-import { useState, useEffect } from 'react';
-//import * as Location from 'expo-location';
-// import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {AsyncStorage} from 'react-native';
+import {useState, useEffect} from 'react';
 import Login from './components/Login';
-// import MainWindow from './components/MainWindow';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import MainWindow from './components/MainWindow';
 import Signup from './components/Signup';
 import Map from './components/Map';
 import ProfileSettings from './components/ProfileSettings';
@@ -23,14 +15,13 @@ import SearchBar from './components/SearchBar';
 import Notifications from './components/Notifications';
 import Missions from './components/Missions';
 import AddPost from './components/AddPost';
-import StackNavigator from '@react-navigation/stack/src/navigators/createStackNavigator';
 import Post from './components/Utils/Post';
 import Experience from './components/Experience';
 import RateLocation from './components/RateLocation';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getPerformance} from "firebase/performance";
+import {initializeApp} from 'firebase/app';
+import {getAnalytics} from 'firebase/analytics';
+import {getPerformance} from 'firebase/performance';
 import * as Sentry from '@sentry/react-native';
 
 const routingInstrumentation = new Sentry.ReactNavigationV4Instrumentation({
@@ -45,7 +36,7 @@ Sentry.init({
   integrations: [
     new Sentry.ReactNativeTracing({
       enableUserInteractionTracing: true,
-      routingInstrumentation
+      routingInstrumentation,
     }),
   ],
   tracesSampleRate: 1.0,
@@ -57,132 +48,22 @@ Sentry.init({
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA71bzuKiNVotAo25DYHL7S93IJP6_h_zo",
-  authDomain: "licentareactnative.firebaseapp.com",
-  projectId: "licentareactnative",
-  storageBucket: "licentareactnative.appspot.com",
-  messagingSenderId: "870675735033",
-  appId: "1:870675735033:web:d726cb1a348c6ccd9e1d28",
-  measurementId: "G-8K8GZMLZQJ"
+  apiKey: 'AIzaSyA71bzuKiNVotAo25DYHL7S93IJP6_h_zo',
+  authDomain: 'licentareactnative.firebaseapp.com',
+  projectId: 'licentareactnative',
+  storageBucket: 'licentareactnative.appspot.com',
+  messagingSenderId: '870675735033',
+  appId: '1:870675735033:web:d726cb1a348c6ccd9e1d28',
+  measurementId: 'G-8K8GZMLZQJ',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const performance = getPerformance(app);
-
-// function arePointsNear(latitude, longitude, centerLatitude, centerLongitude, km) {
-//   var ky = 40000 / 360;
-//   var kx = Math.cos(Math.PI * centerLatitude / 180.0) * ky;
-//   var dx = Math.abs(centerLongitude - longitude) * kx;
-//   var dy = Math.abs(centerLatitude - latitude) * ky;
-//   return Math.sqrt(dx * dx + dy * dy) <= km;
-// }
-
-// function getNearOrFarMessage(locationLatitude, locationLongitude) {
-//   const [location, setLocation] = useState(null);
-//   const [errorMsg, setErrorMsg] = useState(null);
-
-//   useEffect(() => {
-//     (async () => {
-//       let { status } = await Location.requestForegroundPermissionsAsync();
-//       if (status !== 'granted') {
-//         setErrorMsg('Permission to access location was denied');
-//         return;
-//       }
-
-//       let location = await Location.getCurrentPositionAsync({});
-//       setLocation(location);
-//     })();
-//   }, []);
-
-//   let text = 'Waiting..';
-//   let latitude = null;
-//   let longitude = null;
-//   if (errorMsg) {
-//     text = errorMsg;
-//   } else if (location) {
-//     text = JSON.stringify(location);
-//     latitude = text.split(',')[2].split(':')[1];
-//     console.log(latitude);
-//     console.log(longitude);
-//     longitude = text.split(',')[4].split(':')[1];;
-//     console.log(longitude);
-
-//     if(arePointsNear(latitude, longitude, locationLatitude, locationLongitude, 0.2)) {
-//       text = 'You have reached your destination';
-//     }
-//     else {
-//       text = 'You have not reached your destination';
-//     }
-
-//   }
-//   return text;
-// }
-
-// function NearOrFarScreen({route}) {
-//   const { latitude, longitude } = route.params;
-//   let text = getNearOrFarMessage(latitude, longitude);
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center' , justifyContent: 'center' , backgroundColor: 'yellow'}}>
-//       <Text style={{ fontSize: 20, fontFamily: 'Georgia', fontWeight: '100' }} >I'm here!</Text>
-//       <Button title='VALIDATE' onPress={() =>  {alert(text);}}/>
-//     </View>
-//   );
-// }
-
-// function MapScreen({navigation}) {
-//     return (
-//     <View style={styles.container}>
-//       <MapView
-//       style={{height: '100%', width: '100%'}}
-//       provider={PROVIDER_GOOGLE}
-//       showsUserLocation={true}
-//       followsUserLocation={true}
-//       region= {{
-//         latitude: 46.567633,
-//         longitude: 23.816061,
-//         latitudeDelta: 5,
-//         longitudeDelta: 5,
-//       }}
-//       >
-
-//       <MapView.Marker
-//         coordinate=
-//         {
-//           {latitude: 46.567633,
-//           longitude: 23.816061,}
-//         }
-//         >
-//         <Callout onPress={e => {navigation.navigate('NearOrFar', {latitude: '46.567633', longitude: '23.816061'}) }}>
-//           <Text>My home</Text>
-//         </Callout>
-//     </MapView.Marker>
-
-//       <MapView.Marker
-//         coordinate=
-//         {
-//           { latitude: 46.5581978486655,
-//             longitude: 23.80892628453484,
-//           }
-//         }
-//         >
-//         <Callout onPress={e => {navigation.navigate('NearOrFar', {latitude: '46.5581978486655', longitude: '23.80892628453484'}) }}>
-//             <Text>Sebi's home</Text>
-//         </Callout>
-
-//       </MapView.Marker>
-
-//       </MapView>
-
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-//
+getAnalytics(app);
+getPerformance(app);
 
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
+createBottomTabNavigator();
 
 function App() {
   const [id, setId] = useState(null);
@@ -190,58 +71,86 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => { //retrieve user id
+    const fetchData = async () => {
+      //retrieve user id
       await getAsyncData().then(result => {
-        console.log(result, "REZULTATTTTTT")
         setId(result);
-        setInitial(result === null ? "Login" : "Feed")
+        setInitial(result === null ? 'Login' : 'Feed');
         setLoading(false);
       });
-    }
+    };
     fetchData();
   }, []);
 
-
   const getAsyncData = async () => {
-    const id = await AsyncStorage.getItem('id')
-    return JSON.parse(id)
-  }
+    const id = await AsyncStorage.getItem('id');
+    return JSON.parse(id);
+  };
 
-  if(loading) {
+  if (loading) {
     return null;
   }
 
   return (
-            <NavigationContainer>
-              <Stack.Navigator initialRouteName={initial}>
-                <Stack.Screen name="Login" component={Login} options={{header: () => null}}/>
-                <Stack.Screen name="Signup" component={Signup}/>
-                <Stack.Screen name="Map" component={Map}/>
-                <Stack.Screen name="Profile" component={ProfileScreen} options={{header: () => null}}/>
-                <Stack.Screen name="Feed" component={Feed} options={{header: () => null}}/>
-                <Stack.Screen name="Comments" component={Comments} options={{headerTitle: "Comments", headerTransparent: false}}/>
-                <Stack.Screen name="ProfileSettings" component={ProfileSettings} options={{header: () => null}}/>
-                <Stack.Screen name="Search" component={SearchBar} options={{header: () => null}}/>
-                <Stack.Screen name="Notifications" component={Notifications}/>
-                <Stack.Screen name="Post" component={Post}/>
-                <Stack.Screen name="RateLocation" component={RateLocation} options={{header: () => null}}/>
-                <Stack.Screen name="Missions" component={Missions} options={{header: () => null}}/>
-                <Stack.Screen name="Experience" component={Experience} options={{header: () => null}}/>
-                <Stack.Screen name="AddPost" component={AddPost} options={{headerTitle: "New post", headerTransparent: false}}/>
-              </Stack.Navigator>
-            </NavigationContainer>
-
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={initial}>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{header: () => null}}
+        />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Map" component={Map} />
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{header: () => null}}
+        />
+        <Stack.Screen
+          name="Feed"
+          component={Feed}
+          options={{header: () => null}}
+        />
+        <Stack.Screen
+          name="Comments"
+          component={Comments}
+          options={{headerTitle: 'Comments', headerTransparent: false}}
+        />
+        <Stack.Screen
+          name="ProfileSettings"
+          component={ProfileSettings}
+          options={{header: () => null}}
+        />
+        <Stack.Screen
+          name="Search"
+          component={SearchBar}
+          options={{header: () => null}}
+        />
+        <Stack.Screen name="Notifications" component={Notifications} />
+        <Stack.Screen name="Post" component={Post} />
+        <Stack.Screen
+          name="RateLocation"
+          component={RateLocation}
+          options={{header: () => null}}
+        />
+        <Stack.Screen
+          name="Missions"
+          component={Missions}
+          options={{header: () => null}}
+        />
+        <Stack.Screen
+          name="Experience"
+          component={Experience}
+          options={{header: () => null}}
+        />
+        <Stack.Screen
+          name="AddPost"
+          component={AddPost}
+          options={{headerTitle: 'New post', headerTransparent: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-  // return (
-  //     <>
-  //       <NavigationContainer>
-  //         <Tab.Navigator>
-  //           <Tab.Screen name="Feed" component={Feed}/>
-  //           <Tab.Screen name="Profile" component={ProfileScreen}/>
-  //         </Tab.Navigator>
-  //       </NavigationContainer>
-  //     </>
-  // )
 }
 
 // const App = () => {
@@ -255,18 +164,3 @@ function App() {
 // }
 
 export default Sentry.wrap(App);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  paragraph: {
-    textAlign: 'center',
-    marginTop: '15%',
-    marginLeft: '17%',
-    fontSize: 20,
-  },
-});
--- components/Login.js
+++ components/Login.js
@@ -1,7 +1,6 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {View, Text, StyleSheet, TextInput, Button, Alert} from 'react-native';
-import MainWindow from './MainWindow';
-import { AsyncStorage } from 'react-native';
+import {AsyncStorage} from 'react-native';
 class Login extends React.Component {
   constructor(props) {
     super(props);
@@ -12,60 +11,90 @@ class Login extends React.Component {
   };
 
   current_user = {
-      id: '',
-      username: '',
-      first_name: '',
-      last_name: '',
-      email: '',
-      image: '',
-      about: '',
-      level: '',
-      xp: '',
-  }
-
-
-
-  login = async props => {
-      if(this.state.credentials.password !== '' && this.state.credentials.username !== '') {
-          // --FETCH
-          await fetch(ngrok + '/api/custom-user/' + this.state.credentials.username.toString(), {
-              method: 'GET',
-              headers: {'Content-Type': 'application/json', Accept: 'application/json'},
-              // body: JSON.stringify(this.state.credentials.username),
-          })
-              .then(response => response.json())
-              .then(data => {
-                  if (data === []) {
-                      Alert.alert("A user with this credentials does not exist! ", [{text: "OK"}])
-                  } else {
-                      this.current_user.id = data['id']
-                      this.current_user.first_name = data['first_name']
-                      this.current_user.last_name = data['last_name']
-                      this.current_user.username = data['username']
-                      this.current_user.email = data['email']
-                      this.current_user.about = data['about']
-                      this.current_user.image = data['image']
-                      this.current_user.level = data['level']
-                      this.current_user.xp = data['xp']
-                      AsyncStorage.setItem('id', JSON.stringify(this.current_user.id))
-                      AsyncStorage.setItem('first_name', JSON.stringify(this.current_user.first_name))
-                      AsyncStorage.setItem('last_name', JSON.stringify(this.current_user.last_name))
-                      AsyncStorage.setItem('username', JSON.stringify(this.current_user.username))
-                      AsyncStorage.setItem('email', JSON.stringify(this.current_user.email))
-                      AsyncStorage.setItem('about', JSON.stringify(this.current_user.about))
-                      AsyncStorage.setItem('image', JSON.stringify(this.current_user.image))
-                      AsyncStorage.setItem('level', JSON.stringify(this.current_user.level))
-                      AsyncStorage.setItem('xp', JSON.stringify(this.current_user.xp))
-                      this.props.navigation.navigate('Feed');
-                  }
-              })
-              .catch(err => console.error(err));
-      }
-      else
-          Alert.alert("All fields are required! ", [{text: "OK"}])
-
+    id: '',
+    username: '',
+    first_name: '',
+    last_name: '',
+    email: '',
+    image: '',
+    about: '',
+    level: '',
+    xp: '',
   };
 
+  login = async props => {
+    if (
+      this.state.credentials.password !== '' &&
+      this.state.credentials.username !== ''
+    ) {
+      // --FETCH
+      await fetch(
+        ngrok +
+          '/api/custom-user/' +
+          this.state.credentials.username.toString(),
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+          // body: JSON.stringify(this.state.credentials.username),
+        },
+      )
+        .then(response => response.json())
+        .then(data => {
+          if (!data.length()) {
+            Alert.alert('A user with this credentials does not exist! ', [
+              {text: 'OK'},
+            ]);
+          } else {
+            this.current_user.id = data.id;
+            this.current_user.first_name = data.first_name;
+            this.current_user.last_name = data.last_name;
+            this.current_user.username = data.username;
+            this.current_user.email = data.email;
+            this.current_user.about = data.about;
+            this.current_user.image = data.image;
+            this.current_user.level = data.level;
+            this.current_user.xp = data.xp;
+            AsyncStorage.setItem('id', JSON.stringify(this.current_user.id));
+            AsyncStorage.setItem(
+              'first_name',
+              JSON.stringify(this.current_user.first_name),
+            );
+            AsyncStorage.setItem(
+              'last_name',
+              JSON.stringify(this.current_user.last_name),
+            );
+            AsyncStorage.setItem(
+              'username',
+              JSON.stringify(this.current_user.username),
+            );
+            AsyncStorage.setItem(
+              'email',
+              JSON.stringify(this.current_user.email),
+            );
+            AsyncStorage.setItem(
+              'about',
+              JSON.stringify(this.current_user.about),
+            );
+            AsyncStorage.setItem(
+              'image',
+              JSON.stringify(this.current_user.image),
+            );
+            AsyncStorage.setItem(
+              'level',
+              JSON.stringify(this.current_user.level),
+            );
+            AsyncStorage.setItem('xp', JSON.stringify(this.current_user.xp));
+            this.props.navigation.navigate('Feed');
+          }
+        })
+        .catch(err => console.error(err));
+    } else {
+      Alert.alert('All fields are required! ', [{text: 'OK'}]);
+    }
+  };
 
   render() {
     return (
@@ -73,24 +102,27 @@ class Login extends React.Component {
         <Text style={styles.title}> Login user </Text>
         <Text style={styles.text}> Username:</Text>
         <TextInput
-            autoCapitalize='none'
-             style={styles.input}
-             onChangeText={text => (this.state.credentials.username = text)}
+          autoCapitalize="none"
+          style={styles.input}
+          onChangeText={text => (this.state.credentials.username = text)}
         />
         <Text style={styles.text}> Password:</Text>
         <TextInput
-            autoCapitalize='none'
-            secureTextEntry={true}
-            style={styles.input}
-            onChangeText={text => (this.state.credentials.password = text)}
+          autoCapitalize="none"
+          secureTextEntry={true}
+          style={styles.input}
+          onChangeText={text => (this.state.credentials.password = text)}
         />
-        <Button
-          title="LOG IN"
-          onPress={() => this.login() }
-        />
+        <Button title="LOG IN" onPress={() => this.login()} />
         <View style={styles.view}>
-            <Text style = {{fontSize: 18, marginTop: 8}}> Don't have an account? </Text>
-            <Button title="SIGN UP!" onPress={ () => this.props.navigation.navigate('Signup')} />
+          <Text style={{fontSize: 18, marginTop: 8}}>
+            {' '}
+            Don't have an account?{' '}
+          </Text>
+          <Button
+            title="SIGN UP!"
+            onPress={() => this.props.navigation.navigate('Signup')}
+          />
         </View>
       </View>
     );
@@ -114,9 +146,9 @@ const styles = StyleSheet.create({
     fontSize: 30,
   },
   view: {
-      marginTop: 180,
-      flexDirection: 'row',
-      justifyContent: 'center',
+    marginTop: 180,
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
 });
 
